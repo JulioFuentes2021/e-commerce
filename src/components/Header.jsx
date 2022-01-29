2b445bf5129082457f0c3d40ac3454cb98af85f0
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-29 17:12:10 -0600

    Header is working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./styles/css/Header.css";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
+import PcHeader from "./HeaderPc";
 
 const Header = () => {
 	return (
 		<nav>
-			<ul className="links">
+			<PcHeader />
+			<ul className="links links--hiddenMobile">
 				<MenuIcon className="links__item" />
 				<li className="links__item links__item--design">audiophile</li>
-				<ShoppingCartOutlinedIcon className="links__item" />
+				<ShoppingCartOutlinedIcon className="links__item links__item--extraSize" />
 			</ul>
 		</nav>
 	);
---
+++ src/components/HeaderPc.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles/css/Header.css";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const HeaderPc = () => {
+	return (
+		<nav className="links links--hiddenPc">
+			<li className="links__item links__item--design">audiophile</li>
+			<ul className="links__section">
+				<li className="links__item links__item--gap">Home</li>
+				<li className="links__item links__item--gap">Headphones</li>
+				<li className="links__item links__item--gap">Speakers</li>
+				<li className="links__item links__item--gap">Earphones</li>
+			</ul>
+			<ShoppingCartOutlinedIcon className="links__item" />
+		</nav>
+	);
+};
+
+export default HeaderPc;
