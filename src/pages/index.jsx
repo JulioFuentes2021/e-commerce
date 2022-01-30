3f87d53ddd74045a4d0acf21dc94b90b0dace3fc
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-30 15:35:24 -0600

    New product has been created

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import NewProduct from "../components/NewProduct";
+
+const index = () => {
+	return (
+		<div>
+			<Header />
+			<NewProduct />
+		</div>
+	);
+};
+
+export default index;
