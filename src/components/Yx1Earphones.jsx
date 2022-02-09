258ba9e3b06b8c6c4405632734cb1f81359d8528
Julio Fuentes <juliodddddd1234@gmail.com>
2022-02-09 13:22:20 -0600

    Earphones components have been created

---
+++ src/components/Yx1Earphones.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/css/Yx1Earphones.css';
+
+function Yx1Earphones() {
+  return (
+    <article className="earphone">
+      <div className="earphone__imageContainer">
+        <img className="earphone__image" src="https://img.search.brave.com/XlTmXUGXc71M7DHhU1vXmzbVneE8B0jBkJscvjBzFEo/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/ZnJlZXBuZ2ltZy5j/b20vdGh1bWIvdGVj/aG5vbG9neS81MTM5/NC0zLW1vYmlsZS1l/YXJwaG9uZS1waWN0/dXJlLWZyZWUtcGhv/dG8tcG5nLnBuZw" alt="" />
+      </div>
+      <div className="earphone__detail">
+        <span className="earphone__name">YX1 EARPHONES</span>
+        <button type="button" className="earphone__link">SEE PRODUCT</button>
+      </div>
+    </article>
+  );
+}
+
+export default Yx1Earphones;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/Zx7Speaker';
+import App from './components/Yx1Earphones';
 
 ReactDOM.render(
   <React.StrictMode>
