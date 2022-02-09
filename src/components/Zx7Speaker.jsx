e817bffdc0ebfa182ae0769b8c7796d6eaa363d2
Julio Fuentes <juliodddddd1234@gmail.com>
2022-02-09 12:36:41 -0600

    zx7 speaker has been created

---
+++ src/components/Zx7Speaker.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable indent */
+import React from 'react';
+import './styles/css/Zx7Speaker.css';
+
+function Zx7Speaker() {
+  // eslint-disable-next-line indent
+  return (
+    <article className="speaker">
+      <section className="speaker__detail">
+        <span className="speaker__name">ZX7 SPEAKER</span>
+        <button type="button" className="speaker__link">SEE PRODUCT</button>
+      </section>
+      <div className="speaker__imageContainer">
+        <img className="speaker__image" src="https://img.search.brave.com/N8LQkkGjd8nGqjW26Krrg6sl0V_bFfqTZWvRLACX3y4/rs:fit:960:960:1/g:ce/aHR0cHM6Ly9wdXJl/cG5nLmNvbS9wdWJs/aWMvdXBsb2Fkcy9t/ZWRpdW0vcHVyZXBu/Zy5jb20tYXVkaW8t/c3BlYWtlcmF1ZGlv/LXNwZWFrZXJzYXVk/aW9zcGVha2Vyc3Nv/dW5kLWJveHNwZWFr/ZXItMTcwMTUyODMw/OTU1OHRwNmtoLnBu/Zw" alt="" />
+      </div>
+    </article>
+  );
+}
+
+export default Zx7Speaker;
