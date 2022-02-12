3c2e28b1414f998b157c3308ffbacca158357e09
Julio Fuentes <juliodddddd1234@gmail.com>
2022-02-12 16:28:07 -0600

    Index page has been updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from '../components/Header';
 import NewProduct from '../components/NewProduct';
+import GridArticles from '../components/GridArticles';
+import Zx7Speaker from '../components/Zx7Speaker';
+import PremiumSpeaker from '../components/PremiumSpeaker';
+import Yx1Earphones from '../components/Yx1Earphones';
+import '../components/styles/css/index.css';
 
 const index = () => (
-  <div>
+  <div className="index">
     <Header />
     <NewProduct />
+    <main className="main">
+      <div className="main__content">
+        <GridArticles />
+        <PremiumSpeaker />
+        <Zx7Speaker />
+        <Yx1Earphones />
+      </div>
+    </main>
   </div>
 );
 
