9ddceb32b8771f7d1899e8b1a5b8de5b8ec159e7
Julio Fuentes <juliodddddd1234@gmail.com>
2022-05-13 12:21:30 -0600

    FormForSignInOrLogin bug was solved

--- src/components/FormForSignInOrLogin.jsx
+++ src/components/FormForSignInOrLogin.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import Field from '../components/checkout/checkoutForm/FormInputAndLabel'
 
 const FormForSignInOrLogin = () => {
+
+    const handleSubmit = async () => {
+        const url = 'http://localhost:8000/user/sign-up';
+        const data = {
+            username: 'reactuser',
+            gmail: 'react@gmail.com',
+            password: 'react'
+        }
+        try {
+            const response = await fetch(url, {
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+            const data2 = await response.json()
+        } catch (error) {
+            console.log('Ha surgido un error', error)
+        }
+    };
+
     return (
         <div className="account">
             <div className="accountFormContainer">
@@ -11,7 +34,7 @@ const FormForSignInOrLogin = () => {
                     <Field label="Mail" placeholder="address@gmail.com" inputType="mail" />
                     <Field label="Password" placeholder="Your password" inputType="password" />
                 </form>
-                <button className="accountForm__send">Sign In</button>
+                <button onClick={handleSubmit} className="accountForm__send">Sign In</button>
             </div>
         </div>
     )
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Layout from '@components/Layout'
 import Product from '@components/product/ProductContainer'
 
 const ProductPage = () => {
     return (
-        <Layout>
-            <Product />
-        </Layout>
+        <Product />
     )
 }
 
